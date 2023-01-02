cff654d67bdf31f1ba888c3e51cb3c0e5fed56b2
Sheren <sherenshaikh@gmail.com>
2023-01-02 14:43:25 +0000

    Creating new app

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+
+@app.route("/growth/data")
+def growth_data():
+    return "/growth/data"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
